1971964f22df3d2be2c20f1ac9740a0823e87df2
BizzyPythonBear <63131757+BizzyPythonBear@users.noreply.github.com>
2020-09-22 17:04:33 -0700

    python stuff

---
+++ stuff/AIO Python/hello.py
@@ -0,0 +1,48 @@
+import math
+import random
+
+#Basic script for whatever im doing right now, so yeah this is just a fill in.
+#Remove both """ for it to work.
+"""#This variable contains an integer
+quantity = 10
+#This variable contains a float
+unit_price = 1.99
+#This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * 1.99
+#Shows the results
+print("Your total for today's trip is, ", extended_price)
+"""
+
+#This is practice code
+#Remove the """ for it to work.
+"""x = 10
+if x == 0:
+    print("X is zero.")
+else:
+    print("x is ", x)
+print("all done")
+"""
+
+#print("------Absolute Value Finder!------")
+#num = input("Input a negative number.")
+
+#if num < 0:
+    #print(abs(num))
+#else:
+    #print("That is not a negative number")
+
+"""
+x = -4
+y = abs(x)
+print(x)
+print(y)
+"""
+"""
+x = input("Type 81 ")
+float(x)
+print(math.sqrt(x))
+"""
+
+x = 1.23956789795867098560978405967094578
+y = round(x, 2)
+print(y)
